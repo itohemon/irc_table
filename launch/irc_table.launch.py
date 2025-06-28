9a137811ba1c34dfc1e5b04e04d2272aec4467a1
Katsuyuki Itoh <itohemon@gmail.com>
2025-06-28 09:56:41 +0900

    Add turtlebot3 spawn

--- launch/irc_table.launch.py
+++ launch/irc_table.launch.py
@@ -4,9 +4,20 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
 def generate_launch_description():
+    # for spawn turtlebot3
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='-0.25')
+    y_pose = LaunchConfiguration('y_pose', default='-0.25')
+    z_pose = LaunchConfiguration('z_pose', default='0.29')
+
+    # for spawn turtlebot3
+    turtlebot3_gazebo_launch_file_dir = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'launch')
+
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
     world = os.path.join(
         get_package_share_directory('irc_table'),
@@ -30,6 +41,29 @@ def generate_launch_description():
         launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true'}.items()
     )
 
+    # for spawn turtlebot3
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo_launch_file_dir,
+            'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    # for spawn turtlebot3
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('irc_table'),
+                'launch',
+                'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose,
+            'z_pose': z_pose
+        }.items()
+    )
+
     set_env_vars_resources = AppendEnvironmentVariable(
             'GZ_SIM_RESOURCE_PATH',
             os.path.join(
@@ -51,6 +85,8 @@ def generate_launch_description():
     # Add the commands to the launch description
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
+    ld.add_action(spawn_turtlebot_cmd)       # for spawn turtlebot3
+    ld.add_action(robot_state_publisher_cmd) # for spawn turtlebot3
     ld.add_action(set_env_vars_resources)
     ld.add_action(spawn_balls_cmd)
 
